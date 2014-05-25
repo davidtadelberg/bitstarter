543f7ebb0122be1a78335d713646ed7da72638ef
= <=>
2014-05-25 22:13:31 +0000

    Modified index.js
    Changed author

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var fs = require('fs');
-var app = express();
+var htmlfile = "index.html";
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html').toString());
+  response.send(fs.readFileSync(htmlfile).toString());
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+var port = process.env.PORT || 8080;
+
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
